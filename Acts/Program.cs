331479055f68a04ce969970a81f92806dac90bab
Zakhar Aleshkin <gutapuct@mail.ru>
2018-04-08 21:44:41 +0300

    The first commit

---
+++ Acts/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dict = new Dictionary<string, string>();
+
+            dict.Add("Example_1", "Первый пример");
+            dict.Add("Example_2", "Второй пример");
+            dict.Add("Example_3", "Третий пример");
+
+        }
+    }
+}
