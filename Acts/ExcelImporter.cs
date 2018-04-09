f2255322393cebc4edcd42d55c1d1b474e53e715
Zakhar Aleshkin <Zakhar_Aleshkin@epam.com>
2018-04-09 11:13:56 +0300

    Working App, but for different files

---
+++ Acts/ExcelImporter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acts
+{
+    public class ExcelImporter
+    {
+        private string Path { get; }
+        public ExcelImporter()
+        {
+            Path = "C:\\temp\\Values.xlsx";
+        }
+
+        public ExcelImporter(string path)
+        {
+            Path = path;
+        }
+
+        public string[,] GetData()
+        {
+            var excelApp = new Microsoft.Office.Interop.Excel.Application();
+
+            //Книга
+            var workBookExcel = excelApp.Workbooks.Open(Path);
+
+            //Таблица
+            var workSheetExcel = (Microsoft.Office.Interop.Excel.Worksheet)workBookExcel.Sheets[1];
+
+            var lastCell = workSheetExcel.Cells.SpecialCells(Microsoft.Office.Interop.Excel.XlCellType.xlCellTypeLastCell);
+            var result = new string[lastCell.Column, lastCell.Row];
+
+            for (int i = 0; i < (int)lastCell.Column; i++)
+            {
+                for (int j = 0; j < (int)lastCell.Row; j++)
+                {
+                    result[i, j] = workSheetExcel.Cells[j + 1, i + 1].Text.ToString();
+                }
+            }
+
+            workBookExcel.Close(false, Type.Missing, Type.Missing); //закрыть не сохраняя
+            excelApp.Quit(); // вышел из Excel
+            GC.Collect(); // убрал за собой
+
+            return result;
+        }
+    }
+}
--- Acts/Program.cs
+++ Acts/Program.cs
@@ -1,21 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
 
 namespace Acts
 {
     class Program
     {
+        static object _lockObject = new object();
+
         static void Main(string[] args)
         {
-            var dict = new Dictionary<string, string>();
+            var data = new ExcelImporter("D:\\temp\\Values.xlsx").GetData();
 
-            dict.Add("Example_1", "Первый пример");
-            dict.Add("Example_2", "Второй пример");
-            dict.Add("Example_3", "Третий пример");
+            var path = "D:\\temp\\Template.docx";
 
+            for (var val = 1; val < data.GetLength(1); val++)
+            {
+                var dict = new Dictionary<string, string>();
+
+                for (var l = 0; l < data.GetLength(0); l++)
+                {
+                    dict.Add(data[l, 0], data[l, val]);
+                }
+
+                var copypath = $"D:\\temp\\NewDoc_{val}.docx";
+
+                lock (_lockObject)
+                {
+                    File.Copy(path, copypath);
+                }
+
+                using (var doc = WordprocessingDocument.Open(copypath, true))
+                {
+                    var body = doc.MainDocumentPart.Document.Body;
+                    var texts = body.Descendants<Text>();
+
+                    foreach (var pair in dict)
+                    {
+                        //var tokenTexts = texts.Where(t => String.Equals(t.Text, pair.Key));
+                        //foreach (var token in tokenTexts)
+                        //{
+                        //    var parent = token.Parent;
+                        //    var newToken = token.CloneNode(true);
+                        //    var lines = Regex.Split(pair.Value, "\r\n|\r|\n");
+                        //    ((Text)newToken).Text = lines[0];
+                        //    for (int i = 1; i < lines.Length; i++)
+                        //    {
+                        //        parent.AppendChild<Break>(new Break());
+                        //        parent.AppendChild<Text>(new Text(lines[i]));
+                        //    }
+                        //    token.InsertAfterSelf(newToken);
+                        //    token.Remove();
+                        //}
+
+                        var tokenTexts = texts.Where(t => t.Text.Contains(pair.Key));
+                        foreach (var token in tokenTexts)
+                        {
+                            token.Text = token.Text.Replace(pair.Key, pair.Value);
+                        }
+                    }
+
+                    doc.MainDocumentPart.Document.Save();
+                }
+            }
         }
     }
 }
